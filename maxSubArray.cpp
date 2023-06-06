90683feb8a2eeaa0714665971a2caac2ac64b691
progspac-vnn <83080783+progspac-vnn@users.noreply.github.com>
2023-06-06 17:14:37 +0530

    Add files via upload

---
+++ maxSubArray.cpp
@@ -0,0 +1,15 @@
+#include <io.stream>
+
+class Solution{
+public:
+    int maxSubArray(vector<int>& nums){
+        int maxSum = INT_MIN;
+        int curSum = 0
+
+        for(int i=0; i<nums.size(); i++){
+            curSum = max(curSum+nums[i], nums[i]);
+            maxSum = max(curSum,maxSum)
+        }
+        return maxSum;
+    }
+}    
