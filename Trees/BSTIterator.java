ee31e8348b30245a3dfbf41373c2d57ae8448363
progspac-vnn <83080783+progspac-vnn@users.noreply.github.com>
2023-08-04 14:18:18 +0530

    Update BSTIterator.java

--- Trees/BSTIterator.java
+++ Trees/BSTIterator.java
@@ -5,22 +5,22 @@ class BSTIterator {
     public BSTIterator(TreeNode root) {
         st = new Stack<>();
         while(root!=null){
-            st.push(root);
+            st.push(root);  // Get the leftmost node(Inorder)
             root=root.left;
         }
     }
     
     public int next() {
         TreeNode res = st.pop();
-        TreeNode cur = res.right;
+        TreeNode cur = res.right; // Check if any right child exists?
         while(cur!=null){
-            st.push(cur);
-            cur = cur.left;
+            st.push(cur);  // Get the leftmost node(Inorder) [Same logic as that of the BSTIterator in the constructor]
+            cur = cur.left; 
         }
         return res.val;
     }
     
     public boolean hasNext() {
-        return (!st.isEmpty());
+        return (!st.isEmpty()); // if stack is Empty there is no right child
     }
 }
