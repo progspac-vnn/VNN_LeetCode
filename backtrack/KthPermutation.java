5da6ba4706bfcf30de9760a2d70dad39eab75ba5
progspac-vnn <83080783+progspac-vnn@users.noreply.github.com>
2023-09-09 18:38:11 +0530

    Create KthPermutation.java

---
+++ backtrack/KthPermutation.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String getPermutation(int n, int k) {
+        List<Integer> num = new  ArrayList<>();
+        int fact = 1;
+
+        for(int i=1; i<n; i++){
+            fact = fact*i;
+            num.add(i);
+        }
+
+        num.add(n);
+        k=k-1;
+        String ans = "";
+
+        while(true){
+            ans = ans + num.get(k/fact);
+            num.remove(k/fact);
+            
+            if(num.size()==0){
+                break;
+            }
+
+            k = k % fact;
+            fact = fact / num.size();
+            
+        }
+
+        return ans;
+    }
+}
